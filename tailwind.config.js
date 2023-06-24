f2ae7cbbf1880bc3838ea9f57b3aaac587c1200c
Fabrizio2706 <fdkniese@gmail.com>
2023-06-24 17:57:41 +0200

    translated a big part of the css to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,16 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
-  },
+    extend: {
+      fontFamily: {
+        'outfit': ['outfit', 'sans-serif']},
+      },
+      colors: {
+        'light-gray':'#d6e2f0',
+        'gray-blue': '#7b879d',
+        'dark-blue': '#1f3251', 
+      }
+    },
   plugins: [],
 }
 
