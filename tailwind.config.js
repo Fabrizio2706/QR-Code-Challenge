38ad0f3c4e4325ae0e36523e22de6550372a7bbb
Fabrizio2706 <fdkniese@gmail.com>
2023-06-24 18:20:08 +0200

    fixed something?

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,15 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    colors: {
+      'light-gray':'#d6e2f0',
+      'gray-blue': '#7b879d',
+      'dark-blue': '#1f3251', 
+    },
     extend: {
       fontFamily: {
         'outfit': ['outfit', 'sans-serif']},
       },
-      colors: {
-        'light-gray':'#d6e2f0',
-        'gray-blue': '#7b879d',
-        'dark-blue': '#1f3251', 
-      }
     },
   plugins: [],
 }
